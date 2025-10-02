820d8fbc2d0b9ac2f54400003d00bb88e1280cc6
gfloresbdev <floruresgaburieru@gmail.com>
2025-10-01 19:12:42 -0600

    fix deploy

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,14 @@ export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'dist'
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5000',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
   }
 })
