e1299824821d15fca3e5ee59fa5a9ec14b8a3227
gfloresbdev <floruresgaburieru@gmail.com>
2025-10-17 18:57:46 -0600

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,36 +1,17 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
 
 const Navbar = ({ isLoggedIn, onSignOut, devLoggedIn, setDevLoggedIn }) => {
-  const [showMarcasMenu, setShowMarcasMenu] = useState(false);
-
-  // Handler para click en Marcas
-  const handleMarcasClick = () => setShowMarcasMenu((prev) => true);
-
   return (
     <nav className="navbar">
       <div className="navbar-links">
         <Link to="/" className="navbar-link">Home</Link>
-        <Link to="/about" className="navbar-link">AboutUs</Link>
         {isLoggedIn && (
           <>
-            <div
-              className="navbar-link navbar-dropdown"
-              onMouseEnter={() => setShowMarcasMenu(true)}
-              onMouseLeave={() => setShowMarcasMenu(false)}
-              onClick={handleMarcasClick}
-              style={{ userSelect: "none" }} // Elimina position: "relative" aquí, usa la clase CSS
-            >
-              <span style={{ cursor: "pointer" }}>Marcas ▼</span>
-              {showMarcasMenu && (
-                <div className="navbar-dropdown-menu">
-                  <Link to="/mis-marcas" className="navbar-dropdown-item">Mis marcas</Link>
-                  <Link to="/similitudes" className="navbar-dropdown-item">Similitudes</Link>
-                  <Link to="/buscador" className="navbar-dropdown-item">Buscador</Link>
-                </div>
-              )}
-            </div>
+            <Link to="/mis-marcas" className="navbar-link">Mis marcas</Link>
+            <Link to="/similitudes" className="navbar-link">Similitudes</Link>
+            <Link to="/buscador" className="navbar-link">Buscador</Link>
           </>
         )}
         {!isLoggedIn ? (
